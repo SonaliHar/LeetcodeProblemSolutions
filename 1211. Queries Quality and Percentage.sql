2fad0366ce2dd48401bdd7e0381519aa174643a2
SonaliHar <51319066+SonaliHar@users.noreply.github.com>
2025-07-04 17:41:54 -0500

    Update 1211. Queries Quality and Percentage.sql

--- 1211. Queries Quality and Percentage.sql
+++ 1211. Queries Quality and Percentage.sql
@@ -94,3 +94,11 @@ group by  a.query_name,cnt_rating
 
 
 
+--2nd way 
+ select query_name
+,round(avg(rating*1.0 /position),2) as quality ,
+round(avg(case when rating<3 then 1.0 else 0 end)*100,2) as poor_query_percentage 
+from Queries q
+group by query_name
+
+
